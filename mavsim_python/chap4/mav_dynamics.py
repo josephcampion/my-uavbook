cc57a387e2f2bd3fbf4e4ca386acaacb600d5ee1
Joseph Campion <josephcampion@macbook-pro.myfiosgateway.com>
2022-03-14 17:23:39 -0400

    Copy derivatives (kinematics and dynamics) from Chapter 3

--- mavsim_python/chap4/mav_dynamics.py
+++ mavsim_python/chap4/mav_dynamics.py
@@ -121,26 +121,26 @@ class MavDynamics:
         n = forces_moments.item(5)
 
         # position kinematics
-        pos_dot = 
-        north_dot = 
-        east_dot = 
-        down_dot = 
+        pos_dot = Quaternion2Rotation(np.array([e0, e1, e2, e3])) @ np.array([u, v, w])
+        north_dot = pos_dot[0]
+        east_dot = pos_dot[1]
+        down_dot = pos_dot[2]
 
         # position dynamics
-        u_dot = 
-        v_dot = 
-        w_dot = 
+        u_dot = r * v - q * w + fx / MAV.mass
+        v_dot = p * w - r * u + fy / MAV.mass
+        w_dot = q * u - p * v + fz / MAV.mass
 
         # rotational kinematics
-        e0_dot = 
-        e1_dot = 
-        e2_dot = 
-        e3_dot = 
+        e0_dot = 0.5 * (-p*e1 - q*e2 - r*e3)
+        e1_dot = 0.5 * ( p*e0 + r*e2 - q*e3)
+        e2_dot = 0.5 * ( q*e0 - r*e1 + p*e3)
+        e3_dot = 0.5 * ( r*e0 + q*e1 - p*e2)
 
-        # rotational dynamics
-        p_dot = 
-        q_dot = 
-        r_dot = 
+        # rotatonal dynamics
+        p_dot = MAV.gamma1*p*q - MAV.gamma2*q*r             + MAV.gamma3*l + MAV.gamma4*n
+        q_dot = MAV.gamma5*p*r - MAV.gamma6*(p**2 - r**2)   + m / MAV.Jy
+        r_dot = MAV.gamma7*p*q - MAV.gamma1*q*r             + MAV.gamma4*l + MAV.gamma8*n
 
         # collect the derivative of the states
         x_dot = np.array([[north_dot, east_dot, down_dot, u_dot, v_dot, w_dot,
